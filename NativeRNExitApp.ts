031fb049560929673454d78e40364ef3828d83f0
pragyesh <pragyeshsingh71@gmail.com>
2024-12-18 10:27:07 +0530

    fix for web versions also

--- NativeRNExitApp.ts
+++ NativeRNExitApp.ts
@@ -1,8 +1,21 @@
-import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
+import type {TurboModule} from "react-native";
+import {Platform, TurboModuleRegistry} from "react-native";
 
 export interface Spec extends TurboModule {
-    exitApp: () => void;
+  exitApp: () => void;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('RNExitApp');
+const NativeExitApp: Spec | null =
+  Platform.OS !== "web"
+    ? TurboModuleRegistry.getEnforcing<Spec>("RNExitApp")
+    : null;
+
+export default {
+  exitApp: () => {
+    if (NativeExitApp) {
+      NativeExitApp.exitApp();
+    } else {
+      console.warn("exitApp is not supported on the web platform.");
+    }
+  }
+};
